4ef42f5b62aea8d5ed74817c23f0a48657bfccf1
Philipp Wirtenberger <philipp.wirtenberger@actus-tax.com>
2022-06-13 18:28:48 +0200

    build: use typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,46 @@
+const path = require("path");
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, "src", "index.ts"),
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.ts$/,
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
-            query: {
-              presets: [ '@babel/preset-env' ],
+            loader: "ts-loader",
+            options: { transpileOnly: true },
+          },
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-typescript"],
             },
           },
-        ]
+        ],
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(svg)$/,
         use: [
           {
-            loader: 'raw-loader',
-          }
-        ]
-      }
-    ]
+            loader: "raw-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
   },
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js',
-    library: 'Header',
-    libraryTarget: 'umd'
-  }
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+    library: "Header",
+    libraryTarget: "umd",
+  },
 };
